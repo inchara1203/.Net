848778fa764aa9b44a95f12a541a03faba13cefc
inchara1203 <incharab2006@gmail.com>
2026-02-27 12:50:24 +0530

    Add files via upload

---
+++ List of Products/List of Products/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using list_of_products.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace list_of_products.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+            public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { id = 1, Name = "Laptop", Price = 999.99, Quantity = 10 },
+                new Product { id = 2, Name = "Mobile", Price = 45999.99, Quantity = 25 },
+                new Product { id = 3, Name = "HeadPhone", Price = 19999.99, Quantity = 15 }
+            };
+
+
+            return View(products);
+        }
+    }
+}
+ 
---
+++ List of Products/List of Products/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace list_of_products.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
